cad3f0055b9fe84150599e33ff3acd0caa23db02
BunnyyRabbitt <joshtlbt@gmail.com>
2022-10-07 13:59:19 +1100

    UI & Health & Prefab

---
+++ COMP3180-Project/Assets/Scripts/Player/Player Controls/Move.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    void Inputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+}
