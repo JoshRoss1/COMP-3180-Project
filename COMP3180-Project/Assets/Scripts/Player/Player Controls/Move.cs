fb2619492ede1929753c3ea49e24703caf246780
BunnyyRabbitt <joshtlbt@gmail.com>
2022-10-11 17:14:41 +1100

    Crate Open, HealthBar

--- COMP3180-Project/Assets/Scripts/Player/Player Controls/Move.cs
+++ COMP3180-Project/Assets/Scripts/Player/Player Controls/Move.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+
+    [SerializeField]
+    float moveSpeed = 0f;
+
+    private Vector2 moveDirection;
+
     // Start is called before the first frame update
     void Start()
     {
