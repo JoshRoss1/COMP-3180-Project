034dba1b6a9964c65b3c685d2cfd676c713488b1
JoshRoss1 <joshua.ross1@students.mq.edu.au>
2022-10-13 16:20:06 +1100

    Gun Pickup Progress

--- COMP3180-Project/Assets/Scripts/Player/Player Controls/PlayerGeneral.cs
+++ COMP3180-Project/Assets/Scripts/Player/Player Controls/PlayerGeneral.cs
@@ -2,15 +2,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerGeneral : MonoBehaviour
 {
     public float health = 100f;
 
+    private PlayerInput playerControls;
+
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
+        playerControls = GetComponent<PlayerInput>();
+
         
     }
 
@@ -21,5 +26,37 @@ public class PlayerGeneral : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    void ItemPickup(GameObject newItem)
+    {
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            if (transform.GetChild(i).tag == "WeaponSlot")
+            {
+                Instantiate(newItem, transform.GetChild(i));            //Spawn gun that was picked up in hand
+                Destroy(transform.GetChild(i).GetChild(0).gameObject);  //Destroy gun in hand
+                Destroy(newItem);                                       //Destroy gun that was picked up
+                Debug.Log(i);
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Weapon")
+        {
+            playerControls.actions["Interact"].performed += ctx => ItemPickup(collision.gameObject);
+        }
+    }
+
+    private void OnEnable()
+    {
+        playerControls.actions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        playerControls.actions.Disable();
     }
 }
