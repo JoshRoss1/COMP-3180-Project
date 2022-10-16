242a70818b5f7b0fa6476ee91e6942612b543131
BunnyyRabbitt <joshtlbt@gmail.com>
2022-10-16 22:54:54 +1100

    crates

---
+++ COMP3180-Project/Assets/Scripts/EnviroManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviroManager : MonoBehaviour
+{
+
+    public GameObject spawnObject;
+    public Transform[] spawnPoints;
+    private crateControl crate;
+
+    public int crateCount = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(spawnObject, spawnPoints[1].position, Quaternion.identity);
+        Instantiate(spawnObject, spawnPoints[9].position, Quaternion.identity);
+        Instantiate(spawnObject, spawnPoints[6].position, Quaternion.identity);
+        Debug.Log(crateCount);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void crateSpawn()
+    {
+        if (crateCount < 3)
+        {
+            int randomSpawn = Random.Range(0, 9);
+            if (Vector2.Distance(spawnPoints[randomSpawn].position, spawnObject.transform.position) < 1f)
+            {
+
+            }
+            else
+            {
+            Instantiate(spawnObject, spawnPoints[randomSpawn].position, Quaternion.identity);
+            crateCount++;
+            }
+        }
+    }
+
+}
